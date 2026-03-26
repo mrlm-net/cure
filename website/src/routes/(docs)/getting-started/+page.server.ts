25472b5300e736fae734b603ea962614a04d198d
Martin Hrášek <martin.hrasek@drmax.eu>
2026-03-26 08:48:37 +0100

    fix(website): include base path in getting-started redirect
    Use the SvelteKit \`base\` path from \`\$app/paths\` in the redirect so it
    works correctly on GitHub Pages (/cure/docs/installation) as well as
    locally (/docs/installation).

--- website/src/routes/(docs)/getting-started/+page.server.ts
+++ website/src/routes/(docs)/getting-started/+page.server.ts
@@ -1,7 +1,8 @@
 import { redirect } from '@sveltejs/kit';
+import { base } from '$app/paths';
 
 export const prerender = true;
 
 export function load() {
-  throw redirect(301, '/docs/installation');
+  throw redirect(301, `${base}/docs/installation`);
 }
